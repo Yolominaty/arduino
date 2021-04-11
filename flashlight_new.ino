827b9830920bd3dbf9368fd80dd6c05ba5a66dae
Yolominaty <71603904+Yolominaty@users.noreply.github.com>
2021-04-12 01:54:51 +0200

    Add files via upload

---
+++ flashlight_new.ino
@@ -0,0 +1,85 @@
+const int button_pin = 2;
+const int led_pin = 13;
+
+int charge = 5000; // in milli seconds (1000 milliseconds = 1 seconds)
+int max_charge = charge;
+bool button_state = true;
+bool last_button_state = true;
+unsigned long last_millis = 0;
+bool millis_changed = false;
+bool in_use = false;
+bool lamp_on = false;
+bool disabled = false;
+int flicker_time = 2000; // the remaining time of int=charge when the light starts to flicker
+int flicker_frequency = 2;
+int random_number = 0;
+
+
+void setup(){
+  pinMode(button_pin, INPUT_PULLUP);
+  pinMode(led_pin, OUTPUT);
+  //Serial.begin(115200);
+}
+
+void loop(){
+  random_number = random(1, 100);
+  //Serial.println(charge);
+  if(charge < 0){
+    charge = 0;
+  }
+  if(charge > 5000){
+    charge = max_charge;
+  }
+  button_state = digitalRead(button_pin);
+  if(button_state != last_button_state){
+    in_use = 1;
+    if(digitalRead(button_pin) == false){
+      in_use = 1;
+      lamp_on = 1;
+    }else{
+      in_use = 0;
+      lamp_on = 0;
+    }
+  }
+
+  if(lamp_on == true and disabled == false){
+    //Serial.println("xd");
+    digitalWrite(led_pin ,true);
+   }else if(lamp_on == false){
+    digitalWrite(led_pin ,false);
+  }
+
+  if(charge <= flicker_time and in_use == true){
+    if(random_number < flicker_frequency){
+      lamp_on = !lamp_on;
+    }
+  }
+
+  if(in_use == true){
+    if(millis_changed == true){
+      charge --;
+    }
+  }else{
+    if(millis_changed == true){
+      charge ++;
+    }
+  }
+
+  if(charge <= 0){
+    disabled = true;
+    lamp_on = false;
+  }
+
+  if(disabled == true and charge >= max_charge){
+    disabled = false;
+  }
+
+  if(last_millis != millis()){
+    millis_changed = true;
+  }else{
+    millis_changed = false;
+  }
+
+  last_millis = millis();
+  last_button_state = button_state;
+}
